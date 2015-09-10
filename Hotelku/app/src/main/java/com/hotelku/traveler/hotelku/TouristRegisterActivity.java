ace960ee7c832bc3daa9e6788c984f3e317ecf9a
alpro1 <irham10@mhs.if.its.ac.id>
2015-09-10 16:40:57 +0700

    revisi register turis

--- Hotelku/app/src/main/java/com/hotelku/traveler/hotelku/TouristRegisterActivity.java
+++ Hotelku/app/src/main/java/com/hotelku/traveler/hotelku/TouristRegisterActivity.java
@@ -1,7 +1,10 @@
 package com.hotelku.traveler.hotelku;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by KHAIRY on 02-Sep-15.
@@ -12,5 +15,14 @@ public class TouristRegisterActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tourist_register);
+
+        Button registerBtn = (Button) findViewById(R.id.tourist_register_button);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(TouristRegisterActivity.this,TouristRegisterSuccess.class);
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ Hotelku/app/src/main/java/com/hotelku/traveler/hotelku/TouristRegisterSuccess.java
@@ -0,0 +1,16 @@
+package com.hotelku.traveler.hotelku;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Admin on 9/10/2015.
+ */
+public class TouristRegisterSuccess extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tourist_success_register);
+
+    }
+}
