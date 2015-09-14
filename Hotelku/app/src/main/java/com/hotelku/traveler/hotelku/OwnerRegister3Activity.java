cd6f4cbd6b203281b94c97bbc127d15eb6d2fa4a
Novandi Banitama <banitama.novandi11@gmail.com>
2015-09-15 06:54:17 +0700

    finish owner register success

--- Hotelku/app/src/main/java/com/hotelku/traveler/hotelku/OwnerRegister3Activity.java
+++ Hotelku/app/src/main/java/com/hotelku/traveler/hotelku/OwnerRegister3Activity.java
@@ -1,9 +1,54 @@
 package com.hotelku.traveler.hotelku;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by noba on 9/12/2015.
  */
 public class OwnerRegister3Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owner_register_3);
+
+        Button addRoomButton = (Button)findViewById(R.id.register_owner_more_room_button);
+        addRoomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Button cancelButton = (Button)findViewById(R.id.register_owner_cancel_button);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Button nextButton = (Button)findViewById(R.id.register_owner_next_button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(OwnerRegister3Activity.this, OwnerRegisterSuccess.class);
+                startActivity(i);
+            }
+        });
+
+        Button deleteRoomButton = (Button)findViewById(R.id.register_owner_property_delete_room_button);
+        deleteRoomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+    }
 }
---
+++ Hotelku/app/src/main/java/com/hotelku/traveler/hotelku/OwnerRegisterSuccess.java
@@ -0,0 +1,15 @@
+package com.hotelku.traveler.hotelku;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by noba on 9/15/2015.
+ */
+public class OwnerRegisterSuccess extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owner_success_register);
+    }
+}
